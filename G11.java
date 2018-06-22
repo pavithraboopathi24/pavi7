31abfe6b8e1097c206e3ef822ae6eead844f8fb8
pavithraboopathi24 <32186708+pavithraboopathi24@users.noreply.github.com>
2018-06-22 15:54:02 +0530

    Create G11.java

---
+++ G11.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class G11
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=(int)Math.pow(a,b);
+        System.out.println(c);
+    }
+}
