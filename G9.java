86e6e3ce9251797f2dbd8641a4980abebb60ab22
pavithraboopathi24 <32186708+pavithraboopathi24@users.noreply.github.com>
2018-06-22 15:42:56 +0530

    Create G9.java

---
+++ G9.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class G9{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println(a[2]);
+    }
+}
